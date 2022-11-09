6b5543ec3fd921dffb9f0f386dcd95657f799684
Yonas <yonashailug@gmail.com>
2022-11-09 15:32:56 -0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-// @unocss-include
-// import 'uno.css'
-// import 'virtual:uno.css'
-
 import App from './App'
 
 document.addEventListener('DOMContentLoaded', render)
